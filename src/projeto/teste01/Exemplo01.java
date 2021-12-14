1e2910dcf4efea64d464dbd61a2cad024f183d08
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2021-12-14 12:41:48 -0300

    Atualização 01

--- src/projeto/teste01/Exemplo01.java
+++ src/projeto/teste01/Exemplo01.java
@@ -51,9 +51,83 @@ public class Exemplo01 {
 			System.out.print("Senha: ");
 			password1 = read.nextInt();
 		}
+		double saldo = 0.0;
+		
+		System.out.printf("\nSeu saldo atual: R$%.2f", saldo );
+		
+		System.out.println("\nGostaria de depositar ou sacar?(S/N)");
+		char resp = read.next().charAt(0);
+		
+		while(resp == 'S' || resp == 's') {
+		
+			System.out.println("\nEscolha uma das opções: ");
+			System.out.println("1 - Depositar\n2 - Saque");
+			int resposta = read.nextInt();
+		
+		
+			if(resposta == 1) {
+				System.out.print("\nQuanto você deseja depositar: ");
+				double depositar = read.nextDouble();
+				saldo += depositar;
+				System.out.printf("Seu saldo atualizado: R$%.2f", saldo);
+			}
+			else {
+				System.out.println("\nQuanto você deseja sacar: ");
+				double saque = read.nextDouble();
+				while(saque > saldo) {
+					System.out.println("\nVocê está tentando sacar um valor acima do que possui!\nTente de novo, por favor.");
+					System.out.print("\nQuanto você deseja sacar: ");
+					saque = read.nextDouble();
+				}
+				saldo -= saque;
+				System.out.printf("\nSeu saldo atualizado: R$%.2f", saldo);
+			}
+			System.out.println("\n\nGostaria de depositar ou sacar novamente?(S/N)");
+			resp = read.next().charAt(0);			
+		}
+		
 		
 		System.out.println("\nSelecione uma das opções:\n1 - Tesouro Direto\n2 - Fundo Imobiliário\n3 - Renda Variavél\n4 - Outros");
+		int resposta = read.nextInt();
 		
+		while(resposta < 1 || resposta > 4) {
+			System.out.println("Você digitou um número inválido. Tente novamente:");
+			System.out.println("\nSelecione uma das opções:\n1 - Tesouro Direto\n2 - Fundo Imobiliário\n3 - Renda Variavél\n4 - Outros");
+			resposta = read.nextInt();
+		}
+		while(resposta == 1 || resposta == 2 || resposta == 3 || resposta == 4) {
+			
+			if(resposta == 1) {
+				
+			}
+			else if (resposta == 2) {
+				
+			}
+			else if(resposta == 3) {
+				
+			}
+			else if(resposta == 4) {
+				
+			}
+			System.out.println("\nDeseja verificar mais alguma opção de investimento?(S/N)");
+			resp = read.next().charAt(0);
+			
+			if(resp == 's' || resp == 'S') {
+			
+				System.out.println("\n\nSelecione uma das opções:\n1 - Tesouro Direto\n2 - Fundo Imobiliário\n3 - Renda Variavél\n4 - Outros");
+				resposta = read.nextInt();
+			
+				while(resposta < 1 || resposta > 4) {
+					System.out.println("Você digitou um número inválido. Tente novamente:");
+					System.out.println("\nSelecione uma das opções:\n1 - Tesouro Direto\n2 - Fundo Imobiliário\n3 - Renda Variavél\n4 - Outros");
+					resposta = read.nextInt();
+				}
+			}
+			else {
+				resposta = 0;
+			}
+		}
+			
 		
 		System.out.println();
 		
