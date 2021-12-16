574400938a9bbe4a8d80f17dc8fd14abccb7e2ed
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2021-12-16 11:46:09 -0300

    Adicionado classe Tesouro Direto

---
+++ src/projeto/teste/classe/Investimento.java
@@ -0,0 +1,152 @@
+package projeto.teste.classe;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Investimento {
+
+	
+	private double selic24 = 0.0;
+	private double selic27 = 0.0;
+	private double ipca26 = 0.0;
+	private double ipca29 = 0.0;
+	private double selic24Rende = 0.0;
+	private double selic27Rende = 0.0;
+	private double ipca26Rende = 0.0;
+	private double ipca29Rende = 0.0;
+	private double selic24Total = 0.0;
+	private double selic27Total = 0.0;
+	private double ipca26Total = 0.0;
+	private double ipca29Total = 0.0;
+
+	
+	//saldo - var investir
+	// investir 
+	
+	public void investimentos() {
+		DecimalFormat df = new DecimalFormat("0.00");		
+			
+		System.out.println("Meus investimentos:");
+		String conversao = df.format(selic24Total);
+		String conversao2 = df.format(selic24Rende);
+		System.out.println("\n-Tesouro Selic 2024: 7,75% a.a-\nTotal investido: R$" + conversao + "\nRendimento até ano 2024: R$" + conversao2);
+		conversao = df.format(selic27Total);
+		conversao2 = df.format(selic27Rende);
+		System.out.println("\n-Tesouro Selic 2027: 10,35% a.a-\nTotal investido: R$" + conversao + "\nRendimento até ano 2027: R$" + conversao2);
+		conversao = df.format(ipca26Total);
+		conversao2 = df.format(ipca26Rende);
+		System.out.println("\n-Tesouro IPCA+ 2026: IPCA + 5,26% (Inflação 4,55% a.a)-\nTotal investido: R$" + conversao + "\nRendimento até ano 2026: R$" + conversao2);
+		conversao = df.format(ipca29Total);
+		conversao2 = df.format(ipca29Rende);
+		System.out.println("\n-Tesouro IPCA+ 2029: IPCA + 8,75% (Inflação 4,55% a.a)-\nTotal investido: R$" + conversao + "\nRendimento até ano 2029: R$" + conversao2);
+
+	}
+	
+	public void imprimirTesouro() {
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		
+
+		System.out.println("     TESOURO DIRETO\nSimulador de investimento:");
+		
+		double calculo = (2024 - 2021) * (1000 * 0.0775);
+		String teste = df.format(calculo);
+		System.out.println("\n1 - Tesouro Selic 2024: 7,75% a.a - Investindo R$1000,00 seu dinheiro renderia R$" + teste);
+		
+		calculo = (2027 - 2021) * (1000 * 0.0775);
+		teste = df.format(calculo);
+		System.out.println("2 - Tesouro Selic 2027: 7,75% a.a - Investindo R$1000,00 seu dinheiro renderia R$" + teste);
+		
+		calculo = (2026 - 2021) * (1000 * (0.0455 + 0.0526));
+		teste = df.format(calculo);
+		System.out.println("3 - Tesouro IPCA+ 2026: IPCA + 5,26% (Inflação 4,55% a.a) - Investindo R$1000,00 seu dinheiro renderia R$" + teste);
+		
+		calculo = (2029 - 2021) * (1000 * (0.0455 + 0.0875));
+		teste = df.format(calculo);
+		System.out.println("4 - Tesouro IPCA+ 2029: IPCA + 8,75% (Inflação 4,55% a.a) - Investindo R$1000,00 seu dinheiro renderia R$" + teste);
+	}
+	
+	
+	public void tesouroDireto() {
+		
+		Scanner read = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");			
+		
+		System.out.println("\nVocê deseja investir em alguma das opções?(S/N)");
+		char resp = read.next().charAt(0);		
+		
+		while(resp == 'S' || resp == 's') {
+			double investe;
+			System.out.println("\nQual opção(1 | 2 | 3 | 4): ");
+			int opcao = read.nextInt();
+			
+			while(opcao == 1 || opcao == 2 || opcao == 3 || opcao == 4) {
+				switch(opcao) {
+				case 1:
+					System.out.print("\nQuanto deseja investir: ");
+					investe = read.nextDouble();
+				
+					selic24Total += investe;
+					selic24 = (2024 - 2021) * (investe * 0.0775);
+					selic24Rende += selic24;
+					String teste = df.format(selic24);
+					String teste2 = df.format(investe);
+					System.out.println("\n1 - Tesouro Selic 2024: 7,75% a.a - Investindo R$" + teste2 + " seu rendimento será de R$" + teste);
+					break;
+				
+				case 2:
+					System.out.print("\nQuanto deseja investir: ");
+					investe = read.nextDouble();
+				
+					selic27Total += investe;
+					selic27 = (2027 - 2021) * (investe * 0.0775);
+					selic27Rende += selic27;
+					teste = df.format(selic27);
+					teste2 = df.format(investe);
+					System.out.println("2 - Tesouro Selic 2027: 7,75% a.a - Investindo R$" + teste2 + " seu dinheiro renderia R$" + teste);
+					break;
+			
+				case 3:
+					System.out.print("\nQuanto deseja investir: ");
+					investe = read.nextDouble();
+				
+					ipca26Total += investe;
+					ipca26 = (2026 - 2021) * (investe * (0.0455 + 0.0526));
+					ipca26Rende += ipca26;
+					teste = df.format(ipca26);
+					teste2 = df.format(investe);
+					System.out.println("3 - Tesouro IPCA+ 2026: IPCA + 5,26% (Inflação 4,55% a.a) - Investindo R$" + teste2 + " seu dinheiro renderia R$" + teste);
+					break;
+				
+				case 4:
+					System.out.print("\nQuanto deseja investir: ");
+					investe = read.nextDouble();
+				
+					ipca29Total += investe;
+					ipca29 = (2029 - 2021) * (investe * (0.0455 + 0.0875));
+					ipca29Rende += ipca29;
+					teste = df.format(ipca29);
+					teste2 = df.format(investe);
+					System.out.println("4 - Tesouro IPCA+ 2029: IPCA + 8,75% (Inflação 4,55% a.a) - Investindo R$" + teste2 + " seu dinheiro renderia R$" + teste);
+					break;					
+				}	
+				
+				System.out.println();
+				
+				
+				
+				System.out.println("\nDeseja realizar mais algum investimento?\nSe SIM qual opção(1 | 2 | 3 | 4)\nSe NÃO digite 0");
+				opcao = read.nextInt();				
+			}
+			resp = 'N';
+		}
+		
+		read.close();
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/projeto/teste/classe/TesteInvest.java
@@ -0,0 +1,14 @@
+package projeto.teste.classe;
+
+public class TesteInvest {
+
+	public static void main(String[] args) {
+
+		Investimento inv = new Investimento();
+		
+		inv.imprimirTesouro();
+		inv.tesouroDireto();
+		
+	}
+
+}
